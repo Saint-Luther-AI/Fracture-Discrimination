8ae5fe33dc6cf164214a067d2c07c7883f5f7850
Saint-Luther-AI <79800340+Saint-Luther-AI@users.noreply.github.com>
2022-10-22 00:45:55 +0100

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -4,32 +4,32 @@ from sklearn import metrics
 
 def plot_color():
 
-    CT_clinical_BMD = np.loadtxt(open("Outcome/Tibia/outcome_CT_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
+    CT_clinical_BMD = np.loadtxt(open("Outcome/outcome_CT_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
     fpr, tpr, thresholds = metrics.roc_curve(CT_clinical_BMD[:, 2], CT_clinical_BMD[:, 1], pos_label=1)
 
-    CT_clinical = np.loadtxt(open("Outcome/Tibia/outcome_CT_clinical.csv", "rb"), delimiter=",", skiprows=0)
+    CT_clinical = np.loadtxt(open("Outcome/outcome_CT_clinical.csv", "rb"), delimiter=",", skiprows=0)
     fpr2, tpr2, thresholds2 = metrics.roc_curve(CT_clinical[:, 2], CT_clinical[:, 1], pos_label=1)
 
-    CT = np.loadtxt(open("Outcome/Tibia/outcome_CT.csv", "rb"), delimiter=",", skiprows=0)
+    CT = np.loadtxt(open("Outcome/outcome_CT.csv", "rb"), delimiter=",", skiprows=0)
     fpr3, tpr3, thresholds3 = metrics.roc_curve(CT[:, 2], CT[:, 1], pos_label=1)
 
-    clinical_BMD = np.loadtxt(open("Outcome/Tibia/outcome_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
+    clinical_BMD = np.loadtxt(open("Outcome/outcome_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
     fpr4, tpr4, thresholds4 = metrics.roc_curve(clinical_BMD[:, 2], clinical_BMD[:, 1], pos_label=1)
 
-    clinical = np.loadtxt(open("Outcome/Tibia/outcome_clinical.csv", "rb"), delimiter=",", skiprows=0)
+    clinical = np.loadtxt(open("Outcome/outcome_clinical.csv", "rb"), delimiter=",", skiprows=0)
     fpr5, tpr5, thresholds5 = metrics.roc_curve(clinical[:, 2], clinical[:, 1], pos_label=1)
 
-    BMD = np.loadtxt(open("Outcome/Tibia/outcome_BMD.csv", "rb"), delimiter=",", skiprows=0)
+    BMD = np.loadtxt(open("Outcome/outcome_BMD.csv", "rb"), delimiter=",", skiprows=0)
     fpr6, tpr6, thresholds6 = metrics.roc_curve(BMD[:, 2], BMD[:, 1], pos_label=1)
 
     # plot the ROC curves
     plt.figure()
     plt.plot(fpr, tpr, label='HR-pQCT image data, clinical data and BMD', color='blue', linewidth=4)
-    plt.plot(fpr2, tpr2, label='HR-pQCT image data and clinical data', color='orange', linewidth=2)
-    plt.plot(fpr3, tpr3, label='HR-pQCT image data', color='green', linewidth=2)
-    plt.plot(fpr4, tpr4, label='Clinical data and BMD', color='red', linewidth=2)
-    plt.plot(fpr5, tpr5, label='Clinical data', color='purple', linewidth=2)
-    plt.plot(fpr6, tpr6, label='BMD', color='brown', linewidth=2)
+    plt.plot(fpr2, tpr2, label='HR-pQCT image data and clinical data', color='orange', linewidth=1)
+    plt.plot(fpr3, tpr3, label='HR-pQCT image data', color='green', linewidth=1)
+    plt.plot(fpr4, tpr4, label='Clinical data and BMD', color='red', linewidth=1)
+    plt.plot(fpr5, tpr5, label='Clinical data', color='purple', linewidth=1)
+    plt.plot(fpr6, tpr6, label='BMD', color='brown', linewidth=1)
 
     # plot settings
     plt.plot([0,1],[0,1],'k--')
@@ -42,13 +42,13 @@ def plot_color():
 
 def plot_grey():
 
-    CT_clinical_BMD = np.loadtxt(open("Outcome/Tibia/outcome_CT_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
+    CT_clinical_BMD = np.loadtxt(open("Outcome/outcome_CT_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
     fpr, tpr, thresholds = metrics.roc_curve(CT_clinical_BMD[:, 2], CT_clinical_BMD[:, 1], pos_label=1)
 
-    clinical_BMD = np.loadtxt(open("Outcome/Tibia/outcome_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
+    clinical_BMD = np.loadtxt(open("Outcome/outcome_clinical_BMD.csv", "rb"), delimiter=",", skiprows=0)
     fpr4, tpr4, thresholds4 = metrics.roc_curve(clinical_BMD[:, 2], clinical_BMD[:, 1], pos_label=1)
 
-    BMD = np.loadtxt(open("Outcome/Tibia/outcome_BMD.csv", "rb"), delimiter=",", skiprows=0)
+    BMD = np.loadtxt(open("Outcome/outcome_BMD.csv", "rb"), delimiter=",", skiprows=0)
     fpr6, tpr6, thresholds6 = metrics.roc_curve(BMD[:, 2], BMD[:, 1], pos_label=1)
 
     # plot the ROC curves
@@ -69,5 +69,5 @@ def plot_grey():
 
 if __name__ == '__main__':
 
-    #plot_color()
-    plot_grey()
+    plot_color()
+    #plot_grey()
